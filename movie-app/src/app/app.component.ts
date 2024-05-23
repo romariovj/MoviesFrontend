89d3f139c7f9eda0b126948c8f45fe91729846bb
romariovj <romariovargasj@gmail.com>
2024-05-23 12:03:17 -0500

    style: added boostrap framework

--- movie-app/src/app/app.component.ts
+++ movie-app/src/app/app.component.ts
@@ -2,16 +2,18 @@ import { DOCUMENT } from '@angular/common';
 import { Component, ElementRef, HostListener, Inject, OnInit, Renderer2 } from '@angular/core';
 import { NavigationEnd, Route, Router, RouterOutlet } from '@angular/router';
 import { Subscription, filter } from 'rxjs';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = 0;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
